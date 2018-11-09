7dd875b2917f6d1dcdd6d0345e2688d5a54e5f48
hank2_liu <Hank2_Liu@asus.com>
2018-11-09 15:13:06 +0800

    fix ASH_ATTR.c in order to not create /dev/xxx

--- ASH/ASH_ATTR/ASH_ATTR.c
+++ ASH/ASH_ATTR/ASH_ATTR.c
@@ -23,7 +23,7 @@
 #include <linux/fs.h>
 #include <linux/input/ASH.h>
 
-static int g_devMajor = -1;
+static struct device * g_sensordevice = NULL;
 static struct class *g_property_class = NULL;
 
 /**************************/
@@ -40,7 +40,7 @@ static struct class *g_property_class = NULL;
 #define log(fmt, args...) printk(KERN_INFO "[%s]"fmt,MODULE_NAME,##args)
 #define err(fmt, args...) printk(KERN_ERR "[%s]"fmt,MODULE_NAME,##args)
 
-static int ASH_class_open(struct inode * inode, struct file * file)
+/*static int ASH_class_open(struct inode * inode, struct file * file)
 {
     return 0;
 }
@@ -48,11 +48,11 @@ static int ASH_class_open(struct inode * inode, struct file * file)
 static const struct file_operations ASH_class_fops = {
     .owner      = THIS_MODULE,
     .open       = ASH_class_open,
-};
+};*/
 
-static int create_ASH_chrdev(void)
+/*static int create_ASH_chrdev(void)
 {
-	/*create character device*/
+	
 	g_devMajor = register_chrdev(0, "sensors", &ASH_class_fops);
 	if (g_devMajor < 0) {
 		err("%s: could not get major number\n", __FUNCTION__);
@@ -60,7 +60,7 @@ static int create_ASH_chrdev(void)
 	}
 	
 	return 0;
-}
+}*/
 
 static int create_ASH_class(void)
 {
@@ -80,15 +80,15 @@ static int create_ASH_class(void)
 struct device *ASH_ATTR_device_create(ASH_type type)
 {	
 	int ret = 0;
-	dev_t dev;
+	//dev_t dev;
 	struct device *sensor_dev=NULL;
 
 	/*prepare character device and return for error handling*/
-	if(g_devMajor < 0){
+	/*if(g_devMajor < 0){
 		ret=create_ASH_chrdev();
 		if(ret < 0)
 			return NULL;
-	}	
+	}*/	
 
 	/*prepare sys/class file node and return for error handling*/
 	if(g_property_class == NULL || IS_ERR(g_property_class) ){
@@ -99,64 +99,69 @@ struct device *ASH_ATTR_device_create(ASH_type type)
 
 	switch(type){
 		case psensor:
-			dev = MKDEV(g_devMajor, psensor);
-			sensor_dev = device_create(g_property_class, NULL, dev, NULL, "%s", "psensor");
+			//dev = MKDEV(g_devMajor, psensor);
+			sensor_dev = device_create(g_property_class, NULL, 0, NULL, "%s", "psensor");
 			break;
 		case lsensor:
-			dev = MKDEV(g_devMajor, lsensor);
-			sensor_dev = device_create(g_property_class, NULL, dev, NULL, "%s", "lsensor");
+			//dev = MKDEV(g_devMajor, lsensor);
+			sensor_dev = device_create(g_property_class, NULL, 0, NULL, "%s", "lsensor");
 			break;
 		case frgbsensor:
-			dev = MKDEV(g_devMajor, frgbsensor);
-			sensor_dev = device_create(g_property_class, NULL, dev, NULL, "%s", "frgbsensor");
+			//dev = MKDEV(g_devMajor, frgbsensor);
+			sensor_dev = device_create(g_property_class, NULL, 0, NULL, "%s", "frgbsensor");
 			break;
 		case hallsensor:
-			dev = MKDEV(g_devMajor, hallsensor);
-			sensor_dev = device_create(g_property_class, NULL, dev, NULL, "%s", "hallsensor");
+			//dev = MKDEV(g_devMajor, hallsensor);
+			sensor_dev = device_create(g_property_class, NULL, 0, NULL, "%s", "hallsensor");
 			break;
 		case SAR_sensor:
-			dev = MKDEV(g_devMajor, SAR_sensor);
-			sensor_dev = device_create(g_property_class, NULL, dev, NULL, "%s", "SAR_sensor");
+			//dev = MKDEV(g_devMajor, SAR_sensor);
+			sensor_dev = device_create(g_property_class, NULL, 0, NULL, "%s", "SAR_sensor");
 			break;
 		default:
 			err("%s: Type ERROR.(%d)\n", __FUNCTION__, type);
 
 	}
-	
+
 	if (IS_ERR(sensor_dev)) {
 		ret = PTR_ERR(sensor_dev);
 		err("%s: sensor_dev pointer is ERROR(%d). \n", __FUNCTION__, ret);		
 		return NULL;
 	}	
-	
+
+	g_sensordevice = sensor_dev;
 	return sensor_dev;
 }
 EXPORT_SYMBOL(ASH_ATTR_device_create);
 
 void ASH_ATTR_device_remove(ASH_type type)
 {
-	dev_t dev;
+	//dev_t dev;
+	if(!g_sensordevice){
+		err("sensor device is NULL, so can't remove it");
+		return;
+	}
 	
 	switch(type){
 		case psensor:
-			dev = MKDEV(g_devMajor, psensor);
-			device_destroy(g_property_class, dev);
+			//dev = MKDEV(g_devMajor, psensor);
+			device_unregister(g_sensordevice);
 			break;
 		case lsensor:
-			dev = MKDEV(g_devMajor, lsensor);
-			device_destroy(g_property_class, dev);
+			//dev = MKDEV(g_devMajor, lsensor);
+			device_unregister(g_sensordevice);
 			break;
 		case frgbsensor:
-			dev = MKDEV(g_devMajor, frgbsensor);
-			device_destroy(g_property_class, dev);
+			//dev = MKDEV(g_devMajor, frgbsensor);
+			device_unregister(g_sensordevice);
 			break;
 		case hallsensor:
-			dev = MKDEV(g_devMajor, hallsensor);
-			device_destroy(g_property_class, dev);
+			//dev = MKDEV(g_devMajor, hallsensor);
+			device_unregister(g_sensordevice);
 			break;
 		case SAR_sensor:
-			dev = MKDEV(g_devMajor, SAR_sensor);
-			device_destroy(g_property_class, dev);
+			//dev = MKDEV(g_devMajor, SAR_sensor);
+			device_unregister(g_sensordevice);
 			break;
 		default:
 			err("%s: ASH_ATTR_device_remove Type ERROR.(%d)\n", __FUNCTION__, type);
